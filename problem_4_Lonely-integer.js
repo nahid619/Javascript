9437fa8e967b36ac25512743bcbe0b0534846a15
Nahid <you@example.com>
2025-03-03 23:18:41 +0600

    Daily update

---
+++ problem_4_Lonely-integer.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    // Write your code here
+    a.sort(function(a,b){return(a-b)});
+    let n=a.length;
+    if(n==1)
+    {
+        return a;
+    }
+    for(let i=1; i<n; i++)
+    {
+        let uniqe=a[i];
+        if(uniqe!=a[i-1])
+        {
+            if(uniqe!=a[i+1])
+            {
+                return uniqe;
+            }
+        }
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
